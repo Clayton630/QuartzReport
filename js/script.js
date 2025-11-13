849d4b8a52eace31dd8637d3694f9a31d689dd8b
Clayton630 <93291298+Clayton630@users.noreply.github.com>
2025-11-13 14:10:17 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -510,7 +510,7 @@ categoriesContainer.querySelectorAll("a").forEach((link) => {
       link.addEventListener("animationend", finish);
 
       // ... mais on impose une limite de 350 ms (sécurité iOS)
-      setTimeout(finish, 350);
+      setTimeout(finish, 150);
       return;
     }
 
